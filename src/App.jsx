e6e1e368e0a3d5a04a5e435613b4974191321962
Brenden <brendenseifried@gmail.com>
2022-08-12 09:58:31 -0700

    Landing page created and Routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
+import Landing from './views/Landing';
 
 export default function App() {
   return (
     <>
       <Switch>
-        <Route path="/landing">
+        <Route path="/">
           <Landing />
         </Route>
       </Switch>
