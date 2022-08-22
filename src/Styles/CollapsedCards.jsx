f1aa59ec7bc3dcb2c7262040fad42128185d8825
Brenden <brendenseifried@gmail.com>
2022-08-22 13:08:12 -0700

    all cards lined up in CSS

--- src/Styles/CollapsedCards.jsx
+++ src/Styles/CollapsedCards.jsx
@@ -1,25 +1,28 @@
 import styled from 'styled-components';
 
 export const CollapsedCards = styled.div`
-  //   display: flex;
-  //   flex-direction: column;
-  //   justify-content: center;
-  //   justify-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
   h1 {
     display: flex;
     justify-content: center;
-    padding-top: 10%;
+    // padding-top: 10%;
   }
   h2 {
     display: flex;
     // position: absolute;
     justify-content: center;
-    padding-top: 8%;
+    // padding-top: 8%;
     // left: 10%;
-    // top: 10%;
+    top: 10%;
     // padding-left: 20%;
   }
   h3 {
-    padding-top: 10%;
+    display: flex;
+    justify-content: center;
+
+    // padding-top: 10%;
   }
 `;
