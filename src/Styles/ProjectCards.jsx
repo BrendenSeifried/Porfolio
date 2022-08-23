d56a6259e5b59405951712568cd98b00a2e56bf9
Brenden <brendenseifried@gmail.com>
2022-08-23 14:04:07 -0700

    hover effected added to buttons on projectCardOne

--- src/Styles/ProjectCards.jsx
+++ src/Styles/ProjectCards.jsx
@@ -104,7 +104,47 @@ export const CardContainer = styled.div`
     left: 10%;
   }
   a {
+    
     margin: 80px;
+  }
+ a:hover,
+    a:focus {
+      opacity: 0.75;
+    }
+ 
+
+button {
+  font-size: 1.2cm;
+      background-color: #222;
+      border-radius: 4px;
+      border-style: none;
+      box-sizing: border-box;
+      color: white;
+      cursor: pointer;
+      display: inline-block;
+      font-family: 'Farfetch Basis', 'Helvetica Neue', Arial, sans-serif;
+      line-height: 1.5;
+      margin: 0;
+      max-width: none;
+      min-height: 44px;
+      min-width: 10px;
+      outline: none;
+      overflow: hidden;
+      padding: 9px 20px 8px;
+      position: relative;
+      text-align: center;
+      text-transform: none;
+      user-select: none;
+      -webkit-user-select: none;
+      touch-action: manipulation;
+      width: 20%;
+      height: 120%;
+    }
+
+    button:hover,
+    button:focus {
+      opacity: 0.75;
+    }
   }
 `;
 
